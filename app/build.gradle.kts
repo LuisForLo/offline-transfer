c0dbcb696d9d62754d49bde145bd8e031ca87381
LuisForLo <luiguiri@gmail.com>
2026-08-24 00:09:37 -0400

    build: bump resumable secure transfer to 0.6.0-dev

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.luisforlo.offlinetransfer"
         minSdk = 26
         targetSdk = 37
-        versionCode = 9
-        versionName = "0.5.0-dev"
+        versionCode = 10
+        versionName = "0.6.0-dev"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
