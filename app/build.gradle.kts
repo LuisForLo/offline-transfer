d42a29d39abbcbb2e69a2bf01584709acc7973f0
LuisForLo <luiguiri@gmail.com>
2026-08-23 21:32:04 -0400

    chore: bump app to 0.3.0-dev

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.luisforlo.offlinetransfer"
         minSdk = 26
         targetSdk = 37
-        versionCode = 3
-        versionName = "0.2.1-dev"
+        versionCode = 4
+        versionName = "0.3.0-dev"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
