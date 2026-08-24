d22c231af45a1f7d41e6ad8138f5446886a69ba0
LuisForLo <luiguiri@gmail.com>
2026-08-23 20:55:37 -0400

    chore: bump transfer prototype to 0.2.0-dev

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.luisforlo.offlinetransfer"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "0.1.0-dev"
+        versionCode = 2
+        versionName = "0.2.0-dev"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
