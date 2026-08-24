60cb5e3e8b4b61f99162db044a1f174d6feeb331
LuisForLo <luiguiri@gmail.com>
2026-08-24 07:17:21 -0400

    build: bump Android app to 0.7.0-dev

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.luisforlo.offlinetransfer"
         minSdk = 26
         targetSdk = 37
-        versionCode = 11
-        versionName = "0.6.1-dev"
+        versionCode = 12
+        versionName = "0.7.0-dev"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
