cea1ccb9fde46e0c57932c22cf0a62f563e1b4ab
LuisForLo <luiguiri@gmail.com>
2026-09-14 16:58:27 -0300

    build: bump open-file release to 0.8.3-dev

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.luisforlo.offlinetransfer"
         minSdk = 26
         targetSdk = 37
-        versionCode = 15
-        versionName = "0.8.2-dev"
+        versionCode = 16
+        versionName = "0.8.3-dev"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
