adad62f677e6d42fa23d1ab64ca1edffa8cd2de3
LuisForLo <luiguiri@gmail.com>
2026-09-14 15:40:43 -0300

    build: bump NFC pairing prototype to 0.8.0-dev

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.luisforlo.offlinetransfer"
         minSdk = 26
         targetSdk = 37
-        versionCode = 12
-        versionName = "0.7.0-dev"
+        versionCode = 13
+        versionName = "0.8.0-dev"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
